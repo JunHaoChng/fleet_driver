43675f92e71db18664a9f31b90b80391134365b7
Eugene Meng <eugene_meng@cgh.com.sg>
2020-02-10 01:43:46 -0500

    Print mir positions

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['fleet_driver_mir=fleet_driver_mir.fleet_driver_mir:main','remove_mc=fleet_driver_mir.remove_move_coordinate:main'],
+        'console_scripts': ['fleet_driver_mir=fleet_driver_mir.fleet_driver_mir:main',
+            'remove_mc=fleet_driver_mir.remove_move_coordinate:main',
+            'mir_position_converter=fleet_driver_mir.report_positions_in_rmf_coord:main']
     },
 )
